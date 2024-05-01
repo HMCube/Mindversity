c27b144fedf239cb721c6520629356e44a9c29ae
h-charles <h-charles@onu.edu>
2024-05-01 19:14:41 -0400

    Health exercise bar added
    The bar fills up when you hit the button. Maybe this could be like a healthy habits tracker or something and you can hit it every time you do a healthy habit to fill up the meter? Just a thought.

--- app/src/main/java/com/app/mindversity/HealthFragment.kt
+++ app/src/main/java/com/app/mindversity/HealthFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class HealthFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -25,27 +27,44 @@ class HealthFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_health, container, false)
 
+        val progressBar: ProgressBar = view.findViewById(R.id.workoutBar)
+        val exerciseBttn: Button = view.findViewById(R.id.exerciseCounterBttn)
 
         // Initialize RecyclerView
         val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(activity)
         val items = listOf(
             ListItem("Item 1", "Details for Item 1"),
-            ListItem("Lesson 1", "Information about Lesson 1!"),
-            ListItem("Lesson 2", "Information about Lesson 2!"),
-            ListItem("Lesson 3", "Information about Lesson 3!"),
-            ListItem("Lesson 4", "Information about Lesson 4!"),
-            ListItem("Lesson 5", "Information about Lesson 5!"),
-            ListItem("Lesson 6", "Information about Lesson 6!"),
-            ListItem("Lesson 7", "Information about Lesson 7!"),
-            ListItem("Lesson 8", "Information about Lesson 8!"),
+            ListItem("Lesson 1", "Information about Lesson 1"),
+            ListItem("Lesson 2", "Information about Lesson 2"),
+            ListItem("Lesson 3", "Information about Lesson 3"),
+            ListItem("Lesson 4", "Information about Lesson 4"),
+            ListItem("Lesson 5", "Information about Lesson 5"),
+            ListItem("Lesson 6", "Information about Lesson 6"),
+            ListItem("Lesson 7", "Information about Lesson 7"),
+            ListItem("Lesson 8", "Information about Lesson 8"),
             ListItem("Lesson 9", "Information about Lesson 9!")
         )
         val adapter = ListAdapterH(items)
         recyclerView.adapter = adapter
+
+
+        // Define action for the ExerciseButton
+        exerciseBttn.setOnClickListener {
+
+            if (progressBar.progress < 8) {
+                progressBar.progress++
+            }
+            else{
+                progressBar.progress = 0
+            }
+        }
+
+            // Perform the desired action when the button is clicked
+            // For example, show a toast message
+            Toast.makeText(activity, "Exercise completed!", Toast.LENGTH_SHORT).show()
 
         return view
     }
